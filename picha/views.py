0105737b930b422331b4959f186a6c800377db6c
peter max <petermax2004@gmail.com>
2019-07-18 12:32:09 +0300

    Change the http request to render for easy display of content

--- picha/views.py
+++ picha/views.py
@@ -3,8 +3,8 @@ from django.http  import HttpResponse
 import datetime as dt
 
 # Create your views here.
-# def welcome(request):
-#     return render(request,'welcome.html')
+def welcome(request):
+    return render(request,'welcome.html')
 
 def picha_of_day(request):
     date = dt.date.today()
