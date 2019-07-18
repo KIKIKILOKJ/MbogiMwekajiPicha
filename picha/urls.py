dda6c8a8d06fee77ff8e2744ec919256d0c1aa35
peter max <petermax2004@gmail.com>
2019-07-18 11:58:49 +0300

    Add the archives url pattern

--- picha/urls.py
+++ picha/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.picha_of_day,name='pichaToday')
+    url('^today/$',views.picha_of_day,name='pichaToday'),
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_picha,name = 'pastPicha') ] 
 ]
