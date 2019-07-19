db8c812fbf3582ec06e135030aed84b917f2af92
peter max <petermax2004@gmail.com>
2019-07-19 11:17:58 +0300

    Modify the url of the app

--- picha/urls.py
+++ picha/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.welcome,name = 'welcome'),
-    url('^today/$',views.picha_of_day,name='pichaToday'),
+    url(r'^$',views.picha_of_day,name='pichaToday'),
     url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_picha,name = 'pastPicha') 
 ]
