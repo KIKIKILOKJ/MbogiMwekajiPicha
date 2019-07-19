afb860ed8a0ff144c40b59fcb7a41c3d25a9e8ad
peter max <petermax2004@gmail.com>
2019-07-19 08:54:53 +0300

    Add the location class

--- picha/models.py
+++ picha/models.py
@@ -4,3 +4,18 @@ from django.db import models
 class Image(models.Model):
     name = models.CharField(max_length=30)
     description = models.TextField()
+    location = models.ForeignKey(Location)
+    category = models.ForeignKey(Category)
+    
+class Location(models.Model):
+    photo_location = models.CharField(max_length=50)
+
+    def save_location(self):
+        self.save()
+    
+    def delete_location(self):
+        self.delete()
+    
+    def update_location(self, update):
+        self.photo_location = update
+        self.save()
