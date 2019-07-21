e8bb65f394003be6ea2fa012c60e1a29165c53fa
peter max <petermax2004@gmail.com>
2019-07-21 10:59:04 +0300

    Add the search url

--- picha/urls.py
+++ picha/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns=[
     url(r'^$',views.picha_of_day,name='pichaToday'),
-    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_picha,name = 'pastPicha') 
+    url(r'^archives/(\d{4}-\d{2}-\d{2})/$',views.past_days_picha,name = 'pastPicha'),
+    url(r'^search/', views.search_results, name='search_results')
 ]
